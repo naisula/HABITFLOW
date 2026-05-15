deb1af1d3c787d21498c0a45b8dce06ad0fad0f2
Hazel Nyaruai <349429hnw@gmail.com>
2026-05-15 10:34:54 +0300

    Added Quotes API

---
+++ quotes_api.py
@@ -0,0 +1,23 @@
+import requests
+import streamlit as st
+
+
+@st.cache_data
+def get_quote():
+
+    try:
+
+        response = requests.get(
+            "https://zenquotes.io/api/random"
+        )
+
+        data = response.json()
+
+        quote = data[0]["q"]
+        author = data[0]["a"]
+
+        return f'"{quote}" — {author}'
+
+    except:
+
+        return "Stay consistent. Small progress matters."
